ee34ef5df4fc88396e33d75c92a8390a246b5b9b
wagaya1935 <yuezhiyougui716@gmail.com>
2025-12-16 17:47:55 +0900

    マイグレーションファイルにタイトルとファイルの保存場所を追加

--- src/database/migrations/2025_12_16_044805_create_sounds_table.php
+++ src/database/migrations/2025_12_16_044805_create_sounds_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('sounds', function (Blueprint $table) {
             $table->id();
+            $table->string('title');      
+            $table->string('file_path');
             $table->timestamps();
         });
     }
