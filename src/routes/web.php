e9513f138366bb201b65feb8575f69c6a4cbebcc
wagaya1935 <yuezhiyougui716@gmail.com>
2025-12-16 18:03:56 +0900

    SoundControllerをルーティングに追加し、URLを決定する

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SoundController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sounds/create', [SoundController::class, 'create']); 
+Route::post('/sounds', [SoundController::class, 'store']);
