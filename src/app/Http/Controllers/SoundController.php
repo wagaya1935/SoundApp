530c46d930c1dc7e8607b068058bee3d9044c079
wagaya1935 <yuezhiyougui716@gmail.com>
2025-12-18 10:28:20 +0900

    タグを保存できるように設定・追加

--- src/app/Http/Controllers/SoundController.php
+++ src/app/Http/Controllers/SoundController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Sound;
+use App\Models\Tag;
 
 class SoundController extends Controller
 {
@@ -25,17 +26,35 @@ class SoundController extends Controller
     {
         $request->validate([
             'title' => 'required|max:100',
-            'sound_file' => 'required|file|mimes:mp3,wav,ogg|max:10240', // 10MB以内
+            'sound_file' => 'required|file|mimes:mp3,wav,ogg|max:20480',
+            'tags' => 'nullable|string', 
         ]);
 
         $path = $request->file('sound_file')->store('sounds', 'public');
 
-        Sound::create([
+        $sound = Sound::create([
             'title' => $request->title,
             'file_path' => $path,
         ]);
 
-        return redirect('/sounds/create')->with('success', '音声の投稿に成功しました！');
+        if ($request->tags) {
+            $tagNameArray = preg_split('/[\s ]+/u', $request->tags, -1, PREG_SPLIT_NO_EMPTY);
+
+            $tagNameArray = array_unique($tagNameArray);
+
+            foreach ($tagNameArray as $tagName) {
+                //　#が付いていなければ付ける
+                if (!str_starts_with($tagName, '#')) {
+                    $tagName = '#' . $tagName;
+                }
+
+                $tag = Tag::firstOrCreate(['name' => $tagName]);
+
+                $sound->tags()->attach($tag->id);
+            }
+        }
+
+        return redirect('/sounds/create')->with('success', '曲の投稿しました！');
     }
 
     public function destroy($id)
