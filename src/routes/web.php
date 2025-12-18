f69634c3fa0741e0d3e9b985cab3bcda10ceab45
wagaya1935 <yuezhiyougui716@gmail.com>
2025-12-18 09:49:06 +0900

    認証用のルートをゲストユーザとログインユーザでわけるように設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SoundController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [SoundController::class, 'index'])->name('sounds.index');
 
-Route::get('/sounds/create', [SoundController::class, 'create'])->name('sounds.create');
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/sounds', [SoundController::class, 'store'])->name('sounds.store');
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
-Route::get('/sounds/{id}', [SoundController::class, 'destroy'])->name('sounds.destroy');
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/sounds/create', [SoundController::class, 'create'])->name('sounds.create');
+    
+    Route::post('/sounds', [SoundController::class, 'store'])->name('sounds.store');
+    
+    Route::get('/sounds/{id}', [SoundController::class, 'destroy'])->name('sounds.destroy');
+
+});
