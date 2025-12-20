183da8d4305ac83fcc22a09ea407e34e4143c92f
wagaya1935 <yuezhiyougui716@gmail.com>
2025-12-20 23:39:42 +0900

    Userモデルにsoundsリレーションを追加

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Sound;
 
 class User extends Authenticatable
 {
@@ -49,5 +50,10 @@ class User extends Authenticatable
     public function likes()
     {
         return $this->belongsToMany(Sound::class, 'likes', 'user_id', 'sound_id')->withTimestamps();
+    }
+
+    public function sounds() 
+    {
+        return $this->hasMany(Sound::class);
     }
 }
