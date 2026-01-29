fbeb5fc8a3a3a8fc635301aaed8c993a712ed5b3
wagaya <yuezhiyougui716@gmail.com>
2026-01-29 23:59:02 +0900

    退会リンクを削除

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -67,9 +67,6 @@
                         </button>
                     </form>
 
-                    <a href="{{ route('profile.delete.confirm') }}" class="text-red-400 hover:text-red-600 font-medium ml-2">
-                        退会
-                    </a>
                     @else
                     {{-- 未ログイン --}}
                     <a href="{{ route('login') }}" class="text-gray-600 hover:text-indigo-600 font-medium text-sm">
